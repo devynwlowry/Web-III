05ef67f6ee63c8b74ddfd0e475b2c09b8ec2a7e3
Devyn Lowry <devyn.lowry@uconn.edu>
2019-03-26 20:58:22 -0400

    stuff

--- composer/index.php
+++ composer/index.php
@@ -6,5 +6,13 @@ require "vendor/autoload.php";
 use Location\Coordinate;
 use Location\Distance\Vincenty;
 
+$coordinate1 = Coordinate(37.9715, 23.7267);
+$coordinate2 = Coordinate(41.775928, -72.521500);
+
+$calculate = new Vincenty();
+
+
+echo $calculate = getDistance($coordinate1, $coordinate2);
+
 
 ?>
