6d318ab9655a622a3cfc77347cce3ecf347dc99d
Devyn Lowry <devyn.lowry@uconn.edu>
2019-03-26 21:14:07 -0400

    Finished Assignment

--- composer/index.php
+++ composer/index.php
@@ -6,13 +6,19 @@ require "vendor/autoload.php";
 use Location\Coordinate;
 use Location\Distance\Vincenty;
 
-$coordinate1 = Coordinate(37.9715, 23.7267);
-$coordinate2 = Coordinate(41.775928, -72.521500);
+$coordinate1 = new Coordinate(37.9715, 23.7267);
+$coordinate2 = new Coordinate(41.775928, -72.521500);
 
 $calculate = new Vincenty();
 
+echo "This is what the distance is in Meters: ";
+echo $calculate -> getDistance($coordinate1, $coordinate2);
+echo "<br>";
+echo "And this is what the distance is in Miles: ";
+echo $calculate -> getDistance($coordinate1, $coordinate2) * 0.000621371192;
 
-echo $calculate = getDistance($coordinate1, $coordinate2);
+// $conversion = 0.000621371192;
+
 
 
 ?>
